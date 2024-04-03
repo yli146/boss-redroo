b3e69e2f5f2ec4fe49acbbc985d548cdfe8b37f0
Yif <22314559+yli146@users.noreply.github.com>
2024-04-03 22:13:43 +1100

    boilermaker for assessment App

---
+++ dannyApp/src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { surveyPlugin } from 'survey-vue3-ui'
+import App from './App.vue'
+
+createApp(App).use(surveyPlugin).mount('#app')
