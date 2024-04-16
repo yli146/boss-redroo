98dc127585bd75a66b3f44b322f8aa72292511d9
Yif <22314559+yli146@users.noreply.github.com>
2024-04-16 22:25:31 +1000

    deploy medusa for ecommerce

--- dannyApp/src/main.js
+++
@@ -1,7 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { surveyPlugin } from 'survey-vue3-ui'
-import App from './App.vue'
-
-createApp(App).use(surveyPlugin).mount('#app')
